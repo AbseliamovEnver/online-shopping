f8905e188903204077ad71c1ba0034ba6f3c99a5
Enver <a.enver.com@gmail.com>
2017-10-30 03:01:22 +0200

    Project Setup

---
+++ shoppingbackend/src/main/java/net/enver/App.java
@@ -0,0 +1,13 @@
+package net.enver;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
